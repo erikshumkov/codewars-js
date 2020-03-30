5c9079c33197ec1d18b33deda1813a123d6e1fc6
Erik Shumkov <erikshumkov@gmail.com>
2020-03-30 21:19:25 +0200

    challenge accepted

--- solutions.js
+++ solutions.js
@@ -156,3 +156,14 @@ function squareDigits(num) {
 
   return +numberStr;
 }
+
+// 16 Find the divisors!
+function divisors(integer) {
+  var arr = [];
+  for (let i = 2; i <= integer; i++) {
+    if (integer !== i) {
+      if (integer % i === 0) arr.push(i);
+    }
+  }
+  return arr.length === 0 ? `${integer} is prime` : arr;
+}; 
