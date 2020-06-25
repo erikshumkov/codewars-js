fbd31012459c4a41996aac9b2651f40fd1bcaca8
Erik Shumkov <erikshumkov@gmail.com>
2020-06-25 12:54:34 +0200

    new challenge

--- solutions.js
+++ solutions.js
@@ -369,3 +369,17 @@ function arrayMaximalAdjacentDifference(inputArray) {
   return biggestDiff;
 
 }
+
+// 33 Given a character, check if it represents an odd digit, an even digit or not a digit at all.
+// Example
+// For symbol = '5', the output should be characterParity(symbol) = "odd";
+// For symbol = '8', the output should be characterParity(symbol) = "even";
+// For symbol = 'q', the output should be characterParity(symbol) = "not a digit".
+
+function characterParity(symbol) {
+  if (isNaN(symbol)) {
+    return "not a digit";
+  } else {
+    return +symbol % 2 === 0 ? "even" : "odd";
+  }
+}
