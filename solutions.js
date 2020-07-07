15661bb06ac64bc99a4046d25132a3fc4c35293b
Erik Shumkov <erikshumkov@gmail.com>
2020-07-07 12:06:49 +0200

    new challenge

--- solutions.js
+++ solutions.js
@@ -411,3 +411,27 @@ function evenDigitsOnly(n) {
 
   return check.length > 0 ? false : true;
 }
+
+// 37 
+/*
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function.
+*/
+function factorializeANumber(num) {
+
+  const arr = [];
+
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+
+  return arr.reduce((a, b) => a * b);
+
+}
